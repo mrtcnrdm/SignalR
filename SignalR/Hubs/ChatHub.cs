d5c1b4fc8d0f0126df1b63f13fdfae7a54579a98
Mert Can Erdem <mrtcnrdmii@gmail.com>
2022-09-05 17:34:48 +0300

    Initial commit

---
+++ SignalR/Hubs/ChatHub.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace SignalR.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string username, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", username, message);
+        }
+
+        public async Task SendToUser(string user, string receiverConnectionId, string message)
+        {
+            await Clients.Client(receiverConnectionId).SendAsync("ReceiveMessage", user, message);
+        }
+
+        public string GetConnectionId() => Context.ConnectionId;
+    }
+}
---
+++ SignalR/wwwroot/js/Chat.js
@@ -0,0 +1,53 @@
+﻿
+"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+console.log("conn2", connection);
+
+//Disable send button until connection is established
+document.getElementById("sendButton").disabled = true;
+
+connection.on("ReceiveMessage", function (user, message) {
+    var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    var encodedMsg = user + ": " + msg;
+    var li = document.createElement("li");
+    li.textContent = encodedMsg;
+    document.getElementById("messagesList").appendChild(li);
+});
+
+connection.start().then(function () {    
+    connection.invoke("GetConnectionId").then(function (id) {
+        document.getElementById("connectionId").innerText =id ;
+    });
+    document.getElementById("sendButton").disabled = false;
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+document.getElementById("sendButton").addEventListener("click", function (event) {
+    var user = document.getElementById("userInput").value;
+    var message = document.getElementById("messageInput").value;
+    connection.invoke("SendMessage", user, message).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
+
+document.getElementById("sendToUser").addEventListener("click", function (event) {
+    var user = document.getElementById("userInput").value;
+    var receiverConnectionId = document.getElementById("receiverId").value;
+    var message = document.getElementById("messageInput").value;
+    connection.invoke("SendToUser", user, receiverConnectionId, message).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
+
+function createGuid()  
+{  
+   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {  
+      var r = Math.random()*16|0, v = c === 'x' ? r : (r&0x3|0x8);  
+      return v.toString(16);  
+   });  
+}  
+  
