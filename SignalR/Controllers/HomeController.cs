d25b088b0c40827aa724ded7de81198db5a2d474
Mert Can Erdem <mrtcnrdmii@gmail.com>
2022-09-08 12:20:48 +0300

    Fazlalıklar silinerek daha basit bir görünüme getirildi

--- SignalR/Controllers/HomeController.cs
+++ SignalR/Controllers/HomeController.cs
@@ -1,13 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.Logging;
-using SignalR.Hubs;
-using SignalR.Models;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace SignalR.Controllers
 {
@@ -27,17 +19,6 @@ namespace SignalR.Controllers
         public IActionResult Index2()
         {
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
